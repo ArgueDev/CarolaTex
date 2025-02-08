6e830430362b80b3f2630927f56af2a7f4644fe2
ArgueDev <christianarguelloo97@gmail.com>
2025-02-08 13:06:10 -0500

    Inicio de Proyecto Carolatex

---
+++ src/components/Header.tsx
@@ -0,0 +1,14 @@
+export default function Header() {
+  return (
+      <>
+          <div>
+              <nav>
+                  <a href="">Inicio</a>
+                  <a href="">Nosotros</a>
+                  <a href="">Servicios</a>
+                  <a href="">Contactenos</a>
+              </nav>
+          </div>
+      </>
+  )
+}
