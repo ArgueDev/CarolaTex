e0cef326253297c53035ef362f473d70052b997d
ArgueDev <christianarguelloo97@gmail.com>
2025-02-08 15:21:38 -0500

    Header finalizado

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,31 @@
+import { useState } from "react";
+import { Menu, X } from "lucide-react";
+
 export default function Header() {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
-      <>
-          <div>
-              <nav>
-                  <a href="">Inicio</a>
-                  <a href="">Nosotros</a>
-                  <a href="">Servicios</a>
-                  <a href="">Contactenos</a>
-              </nav>
-          </div>
-      </>
-  )
+    <header className="fixed top-0 left-0 w-full bg-white/80 backdrop-blur-md shadow-md p-4 z-50">
+      <div className="container mx-auto flex justify-between items-center">
+        <img src="/img/logo_nav.png" alt="logo" className="w-32 h-auto" />
+        
+        {/* Botón de menú para móviles */}
+        <button className="md:hidden" onClick={() => setIsOpen(!isOpen)}>
+          {isOpen ? <X size={24} /> : <Menu size={24} />}
+        </button>
+
+        {/* Navegación */}
+        <nav
+          className={`md:flex gap-4 absolute md:static bg-white md:bg-transparent w-full left-0 md:w-auto md:flex-row flex-col p-4 md:p-0 top-16 shadow-md md:shadow-none transition-all duration-300 ${
+            isOpen ? "block" : "hidden"
+          }`}
+        >
+          <a href="#" className="block font-bold  py-2 px-4 md:p-0 hover:text-(--verde-menta)">Inicio</a>
+          <a href="#" className="block font-bold  py-2 px-4 md:p-0 hover:text-(--verde-menta)">Nosotros</a>
+          <a href="#" className="block font-bold  py-2 px-4 md:p-0 hover:text-(--verde-menta)">Servicios</a>
+          <a href="#" className="block font-bold  py-2 px-4 md:p-0 hover:text-(--verde-menta)">Contáctenos</a>
+        </nav>
+      </div>
+    </header>
+  );
 }
--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,7 +1,19 @@
+import Header from "../components/Header";
+
 export default function App() {
   return (
     <>
-      
+      <Header />
+      <div className="relative w-full h-screen">
+        <img src="/img/bg.jpg" alt="background" className="w-full h-full object-cover" />
+        
+        <div className="absolute top-1/2 left-10 right-10 transform -translate-y-1/2 bg-white/70 backdrop-blur-md p-8 rounded-lg shadow-lg max-w-md">
+          <h1 className="text-4xl font-bold text-(--azul-marino)">Diseña tu Estilo</h1>
+          <p className="text-black/70 text-lg mt-4">
+            Explora nuestra colección de diseños para que puedas elegir el que mejor se adapte a tus necesidades, descubre la camiseta perfecta que refleja tu estilo y personalidad.
+          </p>
+        </div>
+      </div>
     </>
-  )
+  );
 }
