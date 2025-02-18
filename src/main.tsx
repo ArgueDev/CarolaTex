ddf3a99d6e4fca2f17e08dc3eaf877cc5b5a7c41
ArgueDev <christianarguelloo97@gmail.com>
2025-02-17 23:42:31 -0500

    Cambio

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,8 @@ import { createRoot } from 'react-dom/client'
 // import './index.css'
 import App from './pages/App.tsx'
 
-import { Analytics } from '@vercel/analytics/next';
-
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
-    <Analytics />
   </StrictMode>,
 )
