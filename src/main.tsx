19f95da03b42f7af7aaaf2a0ade0d135de46079e
ArgueDev <christianarguelloo97@gmail.com>
2025-02-17 23:34:43 -0500

    Analytic

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,11 @@ import { createRoot } from 'react-dom/client'
 // import './index.css'
 import App from './pages/App.tsx'
 
+import { Analytics } from '@vercel/analytics/next';
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
+    <Analytics />
   </StrictMode>,
 )
